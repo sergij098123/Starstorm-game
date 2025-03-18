163719b96026800416bad515cff2713b952fe88f
sergij098123 <sergijkartashov@gmail.com>
2025-03-18 19:09:38 +0200

    Update assembly info and asset metadata; adjust source and destination times for content files and added setup files

--- Starstorm/obj/Debug/net8.0/Starstorm.AssemblyInfo.cs
+++ Starstorm/obj/Debug/net8.0/Starstorm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starstorm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbadd0a2ef810ee381a1f2d77b098101ea7cc02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12edb1650f01200474f513d00460d8858c0a150e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starstorm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starstorm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
